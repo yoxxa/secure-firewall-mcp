b84b653c81a1d942445d68acf94fadc1bcd4e7a4
yoxxa <theyoxxa@protonmail.com>
2025-12-01 05:58:21 +0800

    testing: created test for SDK `audit_log` endpoint

---
+++ tests/sdk/test_audit_log.py
@@ -0,0 +1,19 @@
+import pytest
+from sdk import AsyncFMC
+from sdk.exceptions import AsyncFMCError
+
+@pytest.mark.parametrize(
+    "domain_uuid", 
+    [
+        "e276abec-e0f2-11e3-8169-6d9ed49b625f"
+    ]
+)
+@pytest.mark.asyncio
+async def test_get_audit_logs(
+    domain_uuid: str,
+    fmc_client: AsyncFMC
+):
+    result = await fmc_client.get_audit_records(domain_uuid)
+    # Check if return correct data type 
+    assert result is not None
+    assert isinstance(result, list)
