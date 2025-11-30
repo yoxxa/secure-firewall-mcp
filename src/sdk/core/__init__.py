420e1c40f1215fc8795e37ef0693e93937bf0f76
yoxxa <theyoxxa@protonmail.com>
2025-12-01 07:11:01 +0800

    feature: expose API endpoints to AsyncFMC

--- src/sdk/core/__init__.py
+++ src/sdk/core/__init__.py
@@ -2,12 +2,14 @@ from sdk.endpoints.domain import DomainSDK
 from sdk.endpoints.device import DeviceSDK
 from sdk.endpoints.health_alert import HealthAlertSDK
 from sdk.endpoints.audit_log import AuditLogSDK
+from sdk.endpoints.user import UserSDK
 
 class CoreFMC(
     DomainSDK,
     DeviceSDK,
     HealthAlertSDK,
-    AuditLogSDK
+    AuditLogSDK,
+    UserSDK
 ):
     def __init__(self) -> None:
         pass
