6f2daf818377b31766af1316cb6e23af1a8067de
yoxxa <theyoxxa@protonmail.com>
2025-12-01 08:01:14 +0800

    feature: created `job_history` capacity in SDK

--- src/sdk/core/__init__.py
+++ src/sdk/core/__init__.py
@@ -4,6 +4,7 @@ from sdk.endpoints.health_alert import HealthAlertSDK
 from sdk.endpoints.audit_log import AuditLogSDK
 from sdk.endpoints.user import UserSDK
 from sdk.endpoints.ha_pair import HAPairSDK
+from sdk.endpoints.job_history import JobHistorySDK
 
 class CoreFMC(
     DomainSDK,
@@ -11,7 +12,8 @@ class CoreFMC(
     HealthAlertSDK,
     AuditLogSDK,
     UserSDK,
-    HAPairSDK
+    HAPairSDK,
+    JobHistorySDK
 ):
     def __init__(self) -> None:
         pass
---
+++ src/sdk/endpoints/job_history.py
@@ -0,0 +1,17 @@
+from sdk.core.utilities import SDKUtilities
+from sdk.exceptions import AsyncFMCError
+
+class JobHistorySDK(SDKUtilities):
+    def __init__(self) -> None:
+        pass
+
+    async def get_daily_job_history(self) -> list[dict]:
+        """
+        Retrieves all job history for an FMC.
+        Returns:
+            A list[dict] representing job history.
+        """
+        response = await self._request(
+            url = f"/api/fmc_config/v1/domain/{self.global_domain_uuid}/deployment/jobhistories",
+        )
+        return response.json()["items"]
