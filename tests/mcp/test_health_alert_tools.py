f94428acb53b11dee0061f0513eac0279358c0d7
yoxxa <theyoxxa@protonmail.com>
2025-12-06 01:38:34 +0800

    testing: validate `health_alerts` is functional from an SDK and MCP perspective

--- tests/mcp/test_health_alert_tools.py
+++ tests/mcp/test_health_alert_tools.py
@@ -4,21 +4,12 @@ from fastmcp.client import Client
 from fastmcp.client.transports import StreamableHttpTransport
 from fastmcp.exceptions import ToolError
 
-@pytest.mark.parametrize(
-    "domain_uuid", 
-    [
-        "e276abec-e0f2-11e3-8169-6d9ed49b625f"
-    ]
-)
 @pytest.mark.asyncio
 async def test_get_health_alerts(
-    domain_uuid: str,
     mcp_client: Client[StreamableHttpTransport]
 ):
     result = await mcp_client.call_tool(
         name="getAllHealthAlerts", arguments=
-        {
-            "domain_uuid": domain_uuid
-        }
+        {}
     )
     assert isinstance(result.data, list)
--- tests/sdk/test_health_alerts.py
+++ tests/sdk/test_health_alerts.py
@@ -2,18 +2,11 @@ import pytest
 from sdk import AsyncFMC
 from sdk.exceptions import AsyncFMCError
 
-@pytest.mark.parametrize(
-    "domain_uuid", 
-    [
-        "e276abec-e0f2-11e3-8169-6d9ed49b625f"
-    ]
-)
 @pytest.mark.asyncio
 async def test_get_all_health_alerts(
-    domain_uuid: str,
     fmc_client: AsyncFMC
 ):
-    result = await fmc_client.get_all_health_alerts(domain_uuid)
+    result = await fmc_client.get_all_health_alerts()
     # Check if return correct data type 
     assert result is not None
     assert isinstance(result, list)
