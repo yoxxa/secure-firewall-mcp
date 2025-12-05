32ebf0ee2658e72aa2d1700115737b7bd3d99de8
yoxxa <theyoxxa@protonmail.com>
2025-12-05 23:22:44 +0800

    feature: align `device` tools to fit new cache based architecture

--- src/tools/device.py
+++ src/tools/device.py
@@ -1,7 +1,9 @@
+from sdk import AsyncFMC
 from sdk.exceptions import AsyncFMCError
+from manager import FMCManager
 from fastmcp import FastMCP, Context
 from fastmcp.exceptions import ToolError
-from sdk.manager import FMCManager
+from itertools import chain
 
 class Device:
     mcp = FastMCP(
@@ -29,7 +31,6 @@ async def register_device_tools(mcp: FastMCP) -> None:
     description = "Retrieves a device from Cisco Secure Firewall by name."
 )
 async def get_device(
-    domain_uuid: str,
     device_name: str,
     ctx: Context | None = None
 ) -> dict:
@@ -41,35 +42,43 @@ async def get_device(
     Returns:
         API response data
     """
+    fmc: AsyncFMC | None = await device.fmc_manager.select_fmc_by_device_name(device_name)
+    if fmc:
+        return await fmc.get_device_by_name(device_name)
     for fmc in device.fmc_manager.fmc_list:
         try:
-            ctx.info(f"Gathering FMC device {device_name}")
-            return await fmc.get_device_by_name(
-                domain_uuid,
-                device_name
-            )
+            data = await fmc.get_device_by_name(device_name)
+            return await device.fmc_manager.update_standalone_cache(data)
         except AsyncFMCError:
-            ctx.error(f"No device found by name {device_name}")
-            raise ToolError
+            pass
+    raise ToolError
 
 @device.mcp.tool(
     name = "getAllDevices",
     description = "Retrieves all devices from Cisco Secure Firewall."
 )
 async def get_devices(
+    fmc_host: str | None = None,
     ctx: Context | None = None
 ) -> list[dict]:
     """
     Gathers all devices from FMC.
     Args:
+        fmc_host: FMC to gather all devices from
         ctx: MCP context
     Returns:
         API response data
     """
+    # Indicates they want to collect for a specific FMC
+    if fmc_host:
+        fmc = [fmc for fmc in device.fmc_manager.fmc_list if fmc.host.strip("https://") == fmc_host]
+        # fmc[0] = AsyncSDK from list comprehension result
+        return await fmc[0].get_all_devices()
+    response = list([])
     for fmc in device.fmc_manager.fmc_list:
         try:
-            ctx.info(f"Gathering devices for domain")
-            return await fmc.get_all_devices()
+            response.extend(await fmc.get_all_devices())
+            ctx.info(f"Gathering devices for {fmc.host}")
         except AsyncFMCError:
-            ctx.error(f"No devices found for {fmc}")
-            raise ToolError
+            pass
+    return response
