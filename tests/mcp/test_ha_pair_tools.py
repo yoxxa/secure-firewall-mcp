5adfe6e79dd461799625a34a55e638d1abc8ef40
yoxxa <theyoxxa@protonmail.com>
2025-12-06 08:07:58 +0800

    testing: `ha_pair` tools and SDK endpoint requests

--- tests/mcp/test_ha_pair_tools.py
+++ tests/mcp/test_ha_pair_tools.py
@@ -5,35 +5,32 @@ from fastmcp.client.transports import StreamableHttpTransport
 from fastmcp.exceptions import ToolError
 
 @pytest.mark.parametrize(
-    "domain_uuid, device_name", 
+    "device_name", 
     [
-        ("e276abec-e0f2-11e3-8169-6d9ed49b625f", "PERTH-FW")
+        "PERTH-FW"
     ]
 )
 @pytest.mark.asyncio
 async def test_get_device_by_name_good_input(
-    domain_uuid: str,
     device_name: str,
     mcp_client: Client[StreamableHttpTransport]
 ):
     result = await mcp_client.call_tool(
         name="getHAPairByName", arguments=
         {
-            "domain_uuid": domain_uuid,
             "device_name": device_name
         }
     )
     assert isinstance(result.data, dict)
 
 @pytest.mark.parametrize(
-    "domain_uuid, device_name", 
+    "device_name", 
     [
-        ("e276abec-e0f2-11e3-8169-6d9ed49b625f", "INVALID_DEVICE")
+        "INVALID_DEVICE"
     ]
 )
 @pytest.mark.asyncio
 async def test_get_device_by_name_bad_input(
-    domain_uuid: str,
     device_name: str,
     mcp_client: Client[StreamableHttpTransport]
 ):
@@ -41,13 +38,12 @@ async def test_get_device_by_name_bad_input(
         result = await mcp_client.call_tool(
             name="getHAPairByName", arguments=
             {
-                "domain_uuid": domain_uuid,
                 "device_name": device_name
             }
         )
 
 @pytest.mark.asyncio
-async def test_get_all_devices(
+async def test_get_all_ha_pairs(
     mcp_client: Client[StreamableHttpTransport]
 ):
     result = await mcp_client.call_tool(
